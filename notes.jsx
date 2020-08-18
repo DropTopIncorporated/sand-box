dfbd7d8000d68da9718e1433f129c6c76d0a82f0
Lucia Brammer <brammerl96@gmail.com>
2020-08-18 10:17:55 -0700

    adding notes for the api requests

---
+++ notes.jsx
@@ -0,0 +1,7 @@
+[{"operationName":"Drops_ChannelDrops_User","variables":{"login":"kitboga","isLoggedIn":false},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"f309b1d517d288074d50d96512059857cc67d8905d1379e414d70f7b981f2618"}}}]
+^^^ this gets us the information we need on each user 
+
+
+[{"operationName":"DirectoryPage_Game","variables":{"name":"fortnite","options":{"sort":"RELEVANCE","recommendationsContext":{"platform":"web"},"requestID":"JIRA-VXP-2397","tags":[]},"sortTypeIsRecency":false,"limit":50},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"5feb6766dc5d70b33ae9a37cda21e1cd7674187cb74f84b4dd3eb69086d9489c"}}}]
+
+^^^ this gets us all the user's streaming at the time for a certain game 
