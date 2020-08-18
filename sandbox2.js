ec23c4020b9043b5bcb864d95ef9d0a4d0dce847
Jpendy <Jpendy256@gmail.com>
2020-08-17 18:09:40 -0700

    commit

--- sandbox2.js
+++ sandbox2.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+const CLIENT_ID = 'kimne78kx3ncx6brgo4mv6wki5h1ko';
 
 const drops = async() => {
   const browser = await puppeteer.launch({ headless: false });
@@ -9,12 +10,11 @@ const drops = async() => {
 
   return page.evaluate(() => {
     const OUR_SELECTOR = '.tw-tower';
-    // const DROPS_SELECTOR = '.drops-badge';
+    const DROPS_SELECTOR = '.drops-badge';
     
-    return document.querySelectorAll('.tw-mg-l-3 h1').innerText;
-    //   .then(game => {
-    //     game.filter(game => game.querySelector('.drops-badge'));
-    //   });
+    return document.querySelectorAll(DROPS_SELECTOR).length;
+    // .filter(game => game.querySelector('.drops-badge'));
+    
 
   })
     .then(data => {
