d81b9b064e43a29a0bf0f0a5f231585745994686
Jpendy <Jpendy256@gmail.com>
2020-08-19 16:38:30 -0700

    tinkered

--- sandbox2.js
+++ sandbox2.js
@@ -3,29 +3,42 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 const CLIENT_ID = 'kimne78kx3ncx6brgo4mv6wki5h1ko';
-
+                
 const cursor1 = 'eyJzIjowLCJkIjpmYWxzZSwidCI6dHJ1ZX0=';
 const cursor2 = 'eyJzIjo5OSwiZCI6ZmFsc2UsInQiOnRydWV9';
+//  eyJzIjo5OSwiZCI6ZmFsc2UsInQiOnRydWV9
 const cursor3 = 'eyJzIjoxOTksImQiOmZhbHNlLCJ0Ijp0cnVlfQ==';
+//  eyJzIjoxOTksImQiOmZhbHNlLCJ0Ijp0cnVlfQ==
+const cursor4 = 'eyJzIjoyOTksImQiOmZhbHNlLCJ0Ijp0cnVlfQ==';
+//  eyJzIjoyOTksImQiOmZhbHNlLCJ0Ijp0cnVlfQ==
+const drops = (cursor1, cursor2, cursor3) => {
+ 
+  return Promise.all([
+    dataFetch(cursor1),
+    dataFetch(cursor2),
+    dataFetch(cursor3),
+    dataFetch(cursor4)
+  ])
+    .then(array => array.flat())
+    .then(data => {
+      const stringData = JSON.stringify(data, null, 2);
+      fs.writeFileSync('gumtree3.json', stringData);
+    });
+};
 
-const body = [{ 'operationName':'BrowsePage_AllDirectories', 'variables':{ 'limit':100, 'options':{ 'recommendationsContext':{ 'platform':'web' }, 'requestID':'JIRA-VXP-2397', 'sort':'VIEWER_COUNT', 'tags':[] }, 'cursor':'eyJzIjowLCJkIjpmYWxzZSwidCI6dHJ1ZX0=' }, 'extensions':{ 'persistedQuery':{ 'version':1, 'sha256Hash':'78957de9388098820e222c88ec14e85aaf6cf844adf44c8319c545c75fd63203' } } }]
-;
-
-const body2 = [{ 'operationName':'BrowsePage_AllDirectories', 'variables':{ 'limit':100, 'options':{ 'recommendationsContext':{ 'platform':'web' }, 'requestID':'JIRA-VXP-2397', 'sort':'VIEWER_COUNT', 'tags':[] }, 'cursor':'eyJzIjo5OSwiZCI6ZmFsc2UsInQiOnRydWV9' }, 'extensions':{ 'persistedQuery':{ 'version':1, 'sha256Hash':'78957de9388098820e222c88ec14e85aaf6cf844adf44c8319c545c75fd63203' } } }];
-
-const drops = () => {
+function dataFetch(cursor) {
   return fetch('https://gql.twitch.tv/gql', {
     method: 'POST',
     headers: {
       'Content-Type': 'text/plain',
-      'Client-Id': CLIENT_ID,
+      'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
       'User-Agent': 'superman'
     },
-    body: JSON.stringify(body)
+    body: JSON.stringify(body(cursor))
   })
     .then(res => res.json())
-    .then(data => {
-      const newData = data[0].data.directoriesWithTags['edges']
+    .then(item => {
+      return item[0].data.directoriesWithTags['edges']
         .filter(item => item.node.activeDropCampaigns.length > 0)
         .map(({ node: { id, displayName, name, avatarURL } }) => {
           return {
@@ -35,12 +48,45 @@ const drops = () => {
             imageUrl: avatarURL
           };
         });
-      
-      const stringData = JSON.stringify(newData, null, 2);
-      fs.writeFileSync('gumtree.json', stringData);
     });
-};
+}
 
+function body(cursor) {
+  return [{ 'operationName':'BrowsePage_AllDirectories', 'variables':{ 'limit':100, 'options':{ 'recommendationsContext':{ 'platform':'web' }, 'requestID':'JIRA-VXP-2397', 'sort':'VIEWER_COUNT', 'tags':[] }, 'cursor':`${cursor}` }, 'extensions':{ 'persistedQuery':{ 'version':1, 'sha256Hash':'78957de9388098820e222c88ec14e85aaf6cf844adf44c8319c545c75fd63203' } } }];
+}
+
+drops(cursor1, cursor2, cursor3, cursor4);
+
+// return `[{ 'operationName':'BrowsePage_AllDirectories', 'variables':{ 'limit':100, 'options':{ 'recommendationsContext':{ 'platform':'web' }, 'requestID':'JIRA-VXP-2397', 'sort':'VIEWER_COUNT', 'tags':[] }, 'cursor':'eyJzIjowLCJkIjpmYWxzZSwidCI6dHJ1ZX0=' }, 'extensions':{ 'persistedQuery':{ 'version':1, 'sha256Hash':'78957de9388098820e222c88ec14e85aaf6cf844adf44c8319c545c75fd63203' } } }]`;
+
+
+// const drops = () => {
+//   return fetch('https://gql.twitch.tv/gql', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'text/plain',
+//       'Client-Id': CLIENT_ID,
+//       'User-Agent': 'superman'
+//     },
+//     body: JSON.stringify(body)
+//   })
+//     .then(res => res.json())
+//     .then(data => {
+//       const newData = data[0].data.directoriesWithTags['edges']
+//         .filter(item => item.node.activeDropCampaigns.length > 0)
+//         .map(({ node: { id, displayName, name, avatarURL } }) => {
+//           return {
+//             id,
+//             displayName,
+//             name,
+//             imageUrl: avatarURL
+//           };
+//         });
+      
+//       const stringData = JSON.stringify(newData, null, 2);
+//       fs.writeFileSync('gumtree.json', stringData);
+//     });
+// };
 // const filterArray = array => {
 //   return array.map(item =>{
 //     return console.log(item.data.directoriesWithTags['edges'].filter(item => item.node.activeDropCampaigns.length > 0));
@@ -69,4 +115,9 @@ const drops = () => {
 //     });
 // };
 
-drops();
+
+// const body = [{ 'operationName':'BrowsePage_AllDirectories', 'variables':{ 'limit':100, 'options':{ 'recommendationsContext':{ 'platform':'web' }, 'requestID':'JIRA-VXP-2397', 'sort':'VIEWER_COUNT', 'tags':[] }, 'cursor':'eyJzIjowLCJkIjpmYWxzZSwidCI6dHJ1ZX0=' }, 'extensions':{ 'persistedQuery':{ 'version':1, 'sha256Hash':'78957de9388098820e222c88ec14e85aaf6cf844adf44c8319c545c75fd63203' } } }]
+// ;
+
+// const body2 = [{ 'operationName':'BrowsePage_AllDirectories', 'variables':{ 'limit':100, 'options':{ 'recommendationsContext':{ 'platform':'web' }, 'requestID':'JIRA-VXP-2397', 'sort':'VIEWER_COUNT', 'tags':[] }, 'cursor':'eyJzIjo5OSwiZCI6ZmFsc2UsInQiOnRydWV9' }, 'extensions':{ 'persistedQuery':{ 'version':1, 'sha256Hash':'78957de9388098820e222c88ec14e85aaf6cf844adf44c8319c545c75fd63203' } } }];
+
